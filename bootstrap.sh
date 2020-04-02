eb61f19c10c543ffda0f1e4657d9a6ffead8644c
Gaetan <gaetan@ictpourtous.com>
2020-04-03 01:29:27 +0200

    Make the bootstrap script more POSIX compliant

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,22 +23,22 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 	if [[ "$REPLY" =~ ^[Yy]$ ]]; then
 		echo "Installing common software..."
 		brew update > /dev/null 2>&1
-		curl -fsSL https://raw.githubusercontent.com/GSquad934/bootstrap/master/macos_common_apps.txt --output ~/macos_common_apps.txt > /dev/null 2>&1
-		curl -fsSL https://raw.githubusercontent.com/GSquad934/bootstrap/master/macos_common_casks.txt --output ~/macos_common_casks.txt > /dev/null 2>&1
-		< ~/macos_common_apps.txt xargs brew install > /dev/null 2>&1
-		< ~/macos_common_casks.txt xargs brew cask install > /dev/null 2>&1
-		rm ~/macos_common*.txt
+		curl -fsSL https://raw.githubusercontent.com/GSquad934/bootstrap/master/macos_common_apps.txt --output "$HOME"/macos_common_apps.txt > /dev/null 2>&1
+		curl -fsSL https://raw.githubusercontent.com/GSquad934/bootstrap/master/macos_common_casks.txt --output "$HOME"/macos_common_casks.txt > /dev/null 2>&1
+		< "$HOME"/macos_common_apps.txt xargs brew install > /dev/null 2>&1
+		< "$HOME"/macos_common_casks.txt xargs brew cask install > /dev/null 2>&1
+		rm "$HOME"/macos_common*.txt
 		echo "Common software installed"
 	fi
 	read -p "Do you want to install work applications? (Y/n) " -n 1 -r
 	echo
 	if [[ "$REPLY" =~ ^[Yy]$ ]]; then
 		echo "Installing work software..."
-		curl -fsSL https://raw.githubusercontent.com/GSquad934/bootstrap/master/macos_work_apps.txt --output ~/macos_work_apps.txt > /dev/null 2>&1
-		curl -fsSL https://raw.githubusercontent.com/GSquad934/bootstrap/master/macos_work_casks.txt --output ~/macos_work_casks.txt > /dev/null 2>&1
+		curl -fsSL https://raw.githubusercontent.com/GSquad934/bootstrap/master/macos_work_apps.txt --output "$HOME"/macos_work_apps.txt > /dev/null 2>&1
+		curl -fsSL https://raw.githubusercontent.com/GSquad934/bootstrap/master/macos_work_casks.txt --output "$HOME"/macos_work_casks.txt > /dev/null 2>&1
 		< macos_work_apps.txt xargs brew install > /dev/null 2>&1
 		< macos_work_casks.txt xargs brew cask install > /dev/null 2>&1
-		rm ~/macos_work*.txt
+		rm "$HOME"/macos_work*.txt
 		echo "Work software installed"
 	fi
 elif [[ "$OSTYPE" == "linux-gnu" ]] && command -v apt > /dev/null 2>&1; then
@@ -47,18 +47,18 @@ elif [[ "$OSTYPE" == "linux-gnu" ]] && command -v apt > /dev/null 2>&1; then
 	if [[ "$REPLY" =~ ^[Yy]$ ]]; then
 		echo "Installing common software..."
 		sudo apt update > /dev/null 2>&1
-		curl -fsSL https://raw.githubusercontent.com/GSquad934/bootstrap/master/debian_common_apps.txt --output ~/debian_common_apps.txt > /dev/null 2>&1
-		< ~/debian_common_apps.txt xargs sudo apt install -y > /dev/null 2>&1
-		rm ~/debian_common*.txt
+		curl -fsSL https://raw.githubusercontent.com/GSquad934/bootstrap/master/debian_common_apps.txt --output "$HOME"/debian_common_apps.txt > /dev/null 2>&1
+		< "$HOME"/debian_common_apps.txt xargs sudo apt install -y > /dev/null 2>&1
+		rm "$HOME"/debian_common*.txt
 		echo "Common software installed"
 	fi
 	read -p "Do you want to install work applications? (Y/n) " -n 1 -r
 	echo
 	if [[ "$REPLY" =~ ^[Yy]$ ]]; then
 		echo "Installing work software..."
-		curl -fsSL https://raw.githubusercontent.com/GSquad934/bootstrap/master/debian_work_apps.txt --output ~/debian_work_apps.txt > /dev/null 2>&1
+		curl -fsSL https://raw.githubusercontent.com/GSquad934/bootstrap/master/debian_work_apps.txt --output "$HOME"/debian_work_apps.txt > /dev/null 2>&1
 		< debian_work_apps.txt xargs brew install > /dev/null 2>&1
-		rm ~/debian_work*.txt
+		rm "$HOME"/debian_work*.txt
 		echo "Work software installed"
 	fi
 elif [[ "$OSTYPE" == "linux-gnu" ]] && command -v apt-get > /dev/null 2>&1; then
@@ -67,18 +67,18 @@ elif [[ "$OSTYPE" == "linux-gnu" ]] && command -v apt-get > /dev/null 2>&1; then
 	if [[ "$REPLY" =~ ^[Yy]$ ]]; then
 		echo "Installing common software..."
 		sudo apt-get update
-		curl -fsSL https://raw.githubusercontent.com/GSquad934/bootstrap/master/debian_common_apps.txt --output ~/debian_common_apps.txt > /dev/null 2>&1
-		< ~/debian_common_apps.txt xargs sudo apt-get install -y > /dev/null 2>&1
-		rm ~/debian_common*.txt
+		curl -fsSL https://raw.githubusercontent.com/GSquad934/bootstrap/master/debian_common_apps.txt --output "$HOME"/debian_common_apps.txt > /dev/null 2>&1
+		< "$HOME"/debian_common_apps.txt xargs sudo apt-get install -y > /dev/null 2>&1
+		rm "$HOME"/debian_common*.txt
 		echo "Common software installed"
 	fi
 	read -p "Do you want to install work applications? (Y/n) " -n 1 -r
 	echo
 	if [[ "$REPLY" =~ ^[Yy]$ ]]; then
 		echo "Installing work software..."
-		curl -fsSL https://raw.githubusercontent.com/GSquad934/bootstrap/master/debian_work_apps.txt --output ~/debian_work_apps.txt > /dev/null 2>&1
-		< ~/debian_work_apps.txt xargs brew install > /dev/null 2>&1
-		rm ~/debian_work*.txt
+		curl -fsSL https://raw.githubusercontent.com/GSquad934/bootstrap/master/debian_work_apps.txt --output "$HOME"/debian_work_apps.txt > /dev/null 2>&1
+		< "$HOME"/debian_work_apps.txt xargs brew install > /dev/null 2>&1
+		rm "$HOME"/debian_work*.txt
 		echo "Work software installed"
 	fi
 elif [[ "$OSTYPE" == "linux-gnu" ]] && command -v yum > /dev/null 2>&1; then
@@ -87,18 +87,18 @@ elif [[ "$OSTYPE" == "linux-gnu" ]] && command -v yum > /dev/null 2>&1; then
 	if [[ "$REPLY" =~ ^[Yy]$ ]]; then
 		echo "Installing common software..."
 		sudo yum update
-		curl -fsSL https://raw.githubusercontent.com/GSquad934/bootstrap/master/redhat_common_apps.txt --output ~/redhat_common_apps.txt > /dev/null 2>&1
-		< ~/redhat_common_apps.txt xargs sudo yum install -y > /dev/null 2>&1
-		rm ~/redhat_common*.txt
+		curl -fsSL https://raw.githubusercontent.com/GSquad934/bootstrap/master/redhat_common_apps.txt --output "$HOME"/redhat_common_apps.txt > /dev/null 2>&1
+		< "$HOME"/redhat_common_apps.txt xargs sudo yum install -y > /dev/null 2>&1
+		rm "$HOME"/redhat_common*.txt
 		echo "Common software installed"
 	fi
 	read -p "Do you want to install work applications? (Y/n) " -n 1 -r
 	echo
 	if [[ "$REPLY" =~ ^[Yy]$ ]]; then
 		echo "Installing work software..."
-		curl -fsSL https://raw.githubusercontent.com/GSquad934/bootstrap/master/redhat_work_apps.txt --output ~/redhat_work_apps.txt > /dev/null 2>&1
-		< ~/redhat_work_apps.txt xargs brew install > /dev/null 2>&1
-		rm ~/redhat_work*.txt
+		curl -fsSL https://raw.githubusercontent.com/GSquad934/bootstrap/master/redhat_work_apps.txt --output "$HOME"/redhat_work_apps.txt > /dev/null 2>&1
+		< "$HOME"/redhat_work_apps.txt xargs brew install > /dev/null 2>&1
+		rm "$HOME"/redhat_work*.txt
 		echo "Work software installed"
 	fi
 elif [[ "$OSTYPE" == "linux-gnu" ]] && command -v pacman > /dev/null 2>&1; then
@@ -107,18 +107,18 @@ elif [[ "$OSTYPE" == "linux-gnu" ]] && command -v pacman > /dev/null 2>&1; then
 	if [[ "$REPLY" =~ ^[Yy]$ ]]; then
 		echo "Installing common software..."
 		sudo pacman -Syyu
-		curl -fsSL https://raw.githubusercontent.com/GSquad934/bootstrap/master/arch_common_apps.txt --output ~/arch_common_apps.txt > /dev/null 2>&1
-		< ~/arch_common_apps.txt xargs sudo pacman -S --noconfirm install > /dev/null 2>&1
-		rm ~/arch_common*.txt
+		curl -fsSL https://raw.githubusercontent.com/GSquad934/bootstrap/master/arch_common_apps.txt --output "$HOME"/arch_common_apps.txt > /dev/null 2>&1
+		< "$HOME"/arch_common_apps.txt xargs sudo pacman -S --noconfirm install > /dev/null 2>&1
+		rm "$HOME"/arch_common*.txt
 		echo "Common software installed"
 	fi
 	read -p "Do you want to install work applications? (Y/n) " -n 1 -r
 	echo
 	if [[ "$REPLY" =~ ^[Yy]$ ]]; then
 		echo "Installing work software..."
-		curl -fsSL https://raw.githubusercontent.com/GSquad934/bootstrap/master/arch_work_apps.txt --output ~/arch_work_apps.txt > /dev/null 2>&1
-		< ~/arch_work_apps.txt xargs brew install > /dev/null 2>&1
-		rm ~/arch_work*.txt
+		curl -fsSL https://raw.githubusercontent.com/GSquad934/bootstrap/master/arch_work_apps.txt --output "$HOME"/arch_work_apps.txt > /dev/null 2>&1
+		< "$HOME"/arch_work_apps.txt xargs brew install > /dev/null 2>&1
+		rm "$HOME"/arch_work*.txt
 		echo "Work software installed"
 	fi
 fi
@@ -175,9 +175,9 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 	sudo spctl --master-disable
 
 	# Disable software quarantine that displays 'Are you sure you want to run...'
-	if [[ $(ls -lhdO ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV2 | awk '{print$5}') != schg ]]; then
-		echo "" > ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV2
-		sudo chflags schg ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV2 > /dev/null 2>&1
+	if [[ $(ls -lhdO "$HOME"/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV2 | awk '{print$5}') != schg ]]; then
+		echo "" > "$HOME"/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV2
+		sudo chflags schg "$HOME"/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV2 > /dev/null 2>&1
 	fi
 
 	# Generate the locate database
@@ -208,96 +208,96 @@ fi
 #===============================================================================
 #
 #             NOTES: For this to work you must have cloned the Github
-#                 repo to your home folder as ~/projects/dotfiles
+#                 repo to your home folder as "$HOME"/projects/dotfiles
 #
 #===============================================================================
 
 #==============
 # Clone the GitHub repository with all wanted dotfiles
 #==============
-if [[ ! -d ~/projects/dotfiles ]]; then
+if [[ ! -d "$HOME"/projects/dotfiles ]]; then
 	echo "Retrieving dotfiles..."
-	git clone --recurse-submodules https://github.com/gsquad934/dotfiles.git ~/projects/dotfiles > /dev/null 2>&1
-	git -C ~/projects/dotfiles submodule foreach --recursive git checkout master > /dev/null 2>&1
+	git clone --recurse-submodules https://github.com/gsquad934/dotfiles.git "$HOME"/projects/dotfiles > /dev/null 2>&1
+	git -C "$HOME"/projects/dotfiles submodule foreach --recursive git checkout master > /dev/null 2>&1
 else
-	git -C ~/projects/dotfiles pull > /dev/null 2>&1
+	git -C "$HOME"/projects/dotfiles pull > /dev/null 2>&1
 fi
 
-if [[ ! -n "$SSH_CLIENT" ]] || [[ ! -n "$SSH_TTY" ]] && [[ ! -d ~/projects/scripts ]]; then
+if [[ ! -n "$SSH_CLIENT" ]] || [[ ! -n "$SSH_TTY" ]] && [[ ! -d "$HOME"/projects/scripts ]]; then
 	echo "Installing custom scripts..."
-	git clone --recurse-submodules https://github.com/gsquad934/scripts.git ~/projects/scripts > /dev/null 2>&1
-	git -C ~/projects/scripts submodule foreach --recursive git checkout master > /dev/null 2>&1
+	git clone --recurse-submodules https://github.com/gsquad934/scripts.git "$HOME"/projects/scripts > /dev/null 2>&1
+	git -C "$HOME"/projects/scripts submodule foreach --recursive git checkout master > /dev/null 2>&1
 fi
 
 #==============
 # Remove and backup all original dotfiles
 #==============
-echo "Backup your current dotfiles to ~/.old-dotfiles..."
+echo "Backup your current dotfiles to $HOME/.old-dotfiles..."
 if [[ ! -d "$HOME"/.old-dotfiles ]]; then
-	mkdir ~/.old-dotfiles > /dev/null 2>&1
+	mkdir "$HOME"/.old-dotfiles > /dev/null 2>&1
 else
-	rm -Rf ~/.old-dotfiles > /dev/null 2>&1
-	mkdir ~/.old-dotfiles > /dev/null 2>&1
+	rm -Rf "$HOME"/.old-dotfiles > /dev/null 2>&1
+	mkdir "$HOME"/.old-dotfiles > /dev/null 2>&1
 fi
-mv ~/.bash_profile ~/.old-dotfiles/bash_profile > /dev/null 2>&1
-mv ~/.bashrc ~/.old-dotfiles/bashrc > /dev/null 2>&1
-mv ~/.gitconfig ~/.old-dotfiles/gitconfig > /dev/null 2>&1
-mv ~/.iterm2 ~/.old-dotfiles/iterm2 > /dev/null 2>&1
-mv ~/.msmtprc ~/.old-dotfiles/msmtprc > /dev/null 2>&1 || mv ~/.config/msmtp ~/.old-dotfiles/msmtp > /dev/null 2>&1
-mv ~/.p10k.zsh ~/.old-dotfiles/p10k.zsh > /dev/null 2>&1
-mv ~/.tmux.conf ~/.old-dotfiles/tmux.conf > /dev/null 2>&1
-mv ~/.vim ~/.old-dotfiles/vim > /dev/null 2>&1
-mv ~/.vimrc ~/.old-dotfiles/vimrc > /dev/null 2>&1
-mv ~/.zshrc ~/.old-dotfiles/zshrc > /dev/null 2>&1
-mv ~/.config/nvim/init.vim ~/.old-dotfiles/zshrc > /dev/null 2>&1
-mv ~/.config/nvim ~/.old-dotfiles/nvim > /dev/null 2>&1
-mv ~/.config/wget ~/.old-dotfiles/wget > /dev/null 2>&1
-mv ~/.config/weechat ~/.old-dotfiles/weechat > /dev/null 2>&1
-mv ~/.config/vifm ~/.old-dotfiles/vifm > /dev/null 2>&1
+mv "$HOME"/.bash_profile ~/.old-dotfiles/bash_profile > /dev/null 2>&1
+mv "$HOME"/.bashrc ~/.old-dotfiles/bashrc > /dev/null 2>&1
+mv "$HOME"/.gitconfig ~/.old-dotfiles/gitconfig > /dev/null 2>&1
+mv "$HOME"/.iterm2 ~/.old-dotfiles/iterm2 > /dev/null 2>&1
+mv "$HOME"/.msmtprc ~/.old-dotfiles/msmtprc > /dev/null 2>&1 || mv ~/.config/msmtp ~/.old-dotfiles/msmtp > /dev/null 2>&1
+mv "$HOME"/.p10k.zsh ~/.old-dotfiles/p10k.zsh > /dev/null 2>&1
+mv "$HOME"/.tmux.conf ~/.old-dotfiles/tmux.conf > /dev/null 2>&1
+mv "$HOME"/.vim ~/.old-dotfiles/vim > /dev/null 2>&1
+mv "$HOME"/.vimrc ~/.old-dotfiles/vimrc > /dev/null 2>&1
+mv "$HOME"/.zshrc ~/.old-dotfiles/zshrc > /dev/null 2>&1
+mv "$HOME"/.config/nvim/init.vim ~/.old-dotfiles/zshrc > /dev/null 2>&1
+mv "$HOME"/.config/nvim ~/.old-dotfiles/nvim > /dev/null 2>&1
+mv "$HOME"/.config/wget ~/.old-dotfiles/wget > /dev/null 2>&1
+mv "$HOME"/.config/weechat ~/.old-dotfiles/weechat > /dev/null 2>&1
+mv "$HOME"/.config/vifm ~/.old-dotfiles/vifm > /dev/null 2>&1
 
 #==============
 # Create symlinks in the home folder
 #==============
 echo "Installing new dotfiles..."
-ln -s ~/projects/dotfiles/gitconfig ~/.gitconfig > /dev/null 2>&1
-ln -s ~/projects/dotfiles/shellconfig/p10k.zsh ~/.p10k.zsh > /dev/null 2>&1
-ln -s ~/projects/dotfiles/vim ~/.vim > /dev/null 2>&1
-ln -s ~/projects/dotfiles/vim/vimrc ~/.vimrc > /dev/null 2>&1
-ln -s ~/projects/dotfiles/shellconfig/bashrc ~/.bashrc > /dev/null 2>&1
-ln -s ~/projects/dotfiles/shellconfig/zshrc ~/.zshrc > /dev/null 2>&1
-ln -s ~/.vim ~/.config/nvim > /dev/null 2>&1
-ln -s ~/.vim/vimrc ~/.config/nvim/init.vim > /dev/null 2>&1
-ln -s ~/projects/dotfiles/config/wget ~/.config/wget > /dev/null 2>&1
-touch ~/.bash_profile && echo "source ~/.bashrc" > ~/.bash_profile
+ln -s "$HOME"/projects/dotfiles/gitconfig ~/.gitconfig > /dev/null 2>&1
+ln -s "$HOME"/projects/dotfiles/shellconfig/p10k.zsh ~/.p10k.zsh > /dev/null 2>&1
+ln -s "$HOME"/projects/dotfiles/vim ~/.vim > /dev/null 2>&1
+ln -s "$HOME"/projects/dotfiles/vim/vimrc ~/.vimrc > /dev/null 2>&1
+ln -s "$HOME"/projects/dotfiles/shellconfig/bashrc ~/.bashrc > /dev/null 2>&1
+ln -s "$HOME"/projects/dotfiles/shellconfig/zshrc ~/.zshrc > /dev/null 2>&1
+ln -s "$HOME"/.vim ~/.config/nvim > /dev/null 2>&1
+ln -s "$HOME"/.vim/vimrc ~/.config/nvim/init.vim > /dev/null 2>&1
+ln -s "$HOME"/projects/dotfiles/config/wget ~/.config/wget > /dev/null 2>&1
+touch "$HOME"/.bash_profile && echo "source ~/.bashrc" > ~/.bash_profile
 if command -v weechat > /dev/null 2>&1; then
-	mkdir ~/.config/weechat
-	ln -s ~/projects/dotfiles/config/weechat/irc.conf ~/.config/weechat/irc.conf > /dev/null 2>&1
-	ln -s ~/projects/dotfiles/config/weechat/perl ~/.config/weechat/perl > /dev/null 2>&1
-	ln -s ~/projects/dotfiles/config/weechat/python ~/.config/weechat/python > /dev/null 2>&1
-	ln -s ~/projects/dotfiles/config/weechat/trigger.conf ~/.config/weechat/trigger.conf > /dev/null 2>&1
-	ln -s ~/projects/dotfiles/config/weechat/weechat.conf ~/.config/weechat/weechat.conf > /dev/null 2>&1
-	ln -s ~/projects/dotfiles/config/weechat/xfer.conf ~/.config/weechat/xfer.conf > /dev/null 2>&1
-	ln -s ~/projects/dotfiles/config/weechat/buflist.conf ~/.config/weechat/buflist.conf > /dev/null 2>&1
-	ln -s ~/projects/dotfiles/config/weechat/colorize_nicks.conf ~/.config/weechat/colorize_nicks.conf > /dev/null 2>&1
-	ln -s ~/projects/dotfiles/config/weechat/fset.conf ~/.config/weechat/fset.conf > /dev/null 2>&1
-	ln -s ~/projects/dotfiles/config/weechat/iset.conf ~/.config/weechat/iset.conf > /dev/null 2>&1
+	mkdir "$HOME"/.config/weechat
+	ln -s "$HOME"/projects/dotfiles/config/weechat/irc.conf ~/.config/weechat/irc.conf > /dev/null 2>&1
+	ln -s "$HOME"/projects/dotfiles/config/weechat/perl ~/.config/weechat/perl > /dev/null 2>&1
+	ln -s "$HOME"/projects/dotfiles/config/weechat/python ~/.config/weechat/python > /dev/null 2>&1
+	ln -s "$HOME"/projects/dotfiles/config/weechat/trigger.conf ~/.config/weechat/trigger.conf > /dev/null 2>&1
+	ln -s "$HOME"/projects/dotfiles/config/weechat/weechat.conf ~/.config/weechat/weechat.conf > /dev/null 2>&1
+	ln -s "$HOME"/projects/dotfiles/config/weechat/xfer.conf ~/.config/weechat/xfer.conf > /dev/null 2>&1
+	ln -s "$HOME"/projects/dotfiles/config/weechat/buflist.conf ~/.config/weechat/buflist.conf > /dev/null 2>&1
+	ln -s "$HOME"/projects/dotfiles/config/weechat/colorize_nicks.conf ~/.config/weechat/colorize_nicks.conf > /dev/null 2>&1
+	ln -s "$HOME"/projects/dotfiles/config/weechat/fset.conf ~/.config/weechat/fset.conf > /dev/null 2>&1
+	ln -s "$HOME"/projects/dotfiles/config/weechat/iset.conf ~/.config/weechat/iset.conf > /dev/null 2>&1
 fi
 if command -v vifm > /dev/null 2>&1; then
-	mkdir ~/.config/vifm
-	ln -s ~/projects/dotfiles/config/vifm/colors ~/.config/vifm/colors > /dev/null 2>&1
-	ln -s ~/projects/dotfiles/config/vifm/vifmrc ~/.config/vifm/vifmrc > /dev/null 2>&1
+	mkdir "$HOME"/.config/vifm
+	ln -s "$HOME"/projects/dotfiles/config/vifm/colors ~/.config/vifm/colors > /dev/null 2>&1
+	ln -s "$HOME"/projects/dotfiles/config/vifm/vifmrc ~/.config/vifm/vifmrc > /dev/null 2>&1
 fi
 if command -v msmtp > /dev/null 2>&1; then
-	ln -s ~/projects/dotfiles/config/msmtp ~/.config/msmtp > /dev/null 2>&1
+	ln -s "$HOME"/projects/dotfiles/config/msmtp ~/.config/msmtp > /dev/null 2>&1
 fi
 if [[ "$OSTYPE" == "darwin"* ]] && [[ -d /Applications/iTerm.app ]]; then
-	ln -s ~/projects/dotfiles/iterm2 ~/.iterm2 > /dev/null 2>&1
+	ln -s "$HOME"/projects/dotfiles/iterm2 ~/.iterm2 > /dev/null 2>&1
 fi
 # If this is a SSH connection, install the server config of TMUX
 if [[ -n "$SSH_CLIENT" ]] || [[ -n "$SSH_TTY" ]]; then
-	ln -s ~/projects/dotfiles/tmux/tmux29-server.conf ~/.tmux.conf > /dev/null 2>&1
+	ln -s "$HOME"/projects/dotfiles/tmux/tmux29-server.conf ~/.tmux.conf > /dev/null 2>&1
 else
-	ln -s ~/projects/dotfiles/tmux/tmux-workstation.conf ~/.tmux.conf > /dev/null 2>&1
+	ln -s "$HOME"/projects/dotfiles/tmux/tmux-workstation.conf ~/.tmux.conf > /dev/null 2>&1
 fi
 
 #==============
