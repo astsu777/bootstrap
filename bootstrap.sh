38167dca3aaa150d9b6204dedf4af572aa8459ad
Gaetan <gaetan@ictpourtous.com>
2021-01-25 21:58:19 +0100

    Add check for Pritunl repository in Pacman configuration

--- bootstrap.sh
+++ bootstrap.sh
@@ -138,10 +138,12 @@ installpythondeps(){
 if type pacman > /dev/null 2>&1; then
 	setupcustomrepos(){
 		# Pritunl
-		sudo tee -a /etc/pacman.conf << EOF
-		[pritunl]
-		Server = https://repo.pritunl.com/stable/pacman
-EOF
+		if ! grep '^\[pritunl\]' /etc/pacman.conf > /dev/null 2>&1; then
+			sudo tee -a /etc/pacman.conf <<-'EOF'
+			[pritunl]
+			Server = https://repo.pritunl.com/stable/pacman
+			EOF
+		fi
 		sudo pacman-key --keyserver hkp://keyserver.ubuntu.com -r 7568D9BB55FF9E5287D586017AE645C0CF8E292A
 		sudo pacman-key --lsign-key 7568D9BB55FF9E5287D586017AE645C0CF8E292A
 	}
